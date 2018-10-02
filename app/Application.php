dedfb573a712f75c64e1005f1d59c17147c323e5
Kerrial Newham <41723430+Kerrialn@users.noreply.github.com>
2018-10-02 11:17:51 +0400

    Update Application.php

--- app/Application.php
+++ app/Application.php
@@ -8,12 +8,13 @@ use App\Job;
 
 class Application extends Model
 {
-    
+    // define Application user (application belongs to user)
     public function user()
     {
         return $this->belongsTo('App\User', 'user_id', 'id');
     }
 
+    // define Application job (Aplpication belongs to job)
     public function job()
     {
         return $this->belongsTo('App\Job', 'job_id', 'id');
